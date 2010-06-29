59725ca8d90f431381c119ec268c956307eb1f2c
renatomefidf <renatomefidf@ddd36b68-90d1-4cec-e3d0-770ee2dfee97>
2010-06-29 17:44:39 +0000

    Formulário agora funcionando com Include(TO DO)

--- trunk/application/modules/contact/controllers/IndexController.php
+++ trunk/application/modules/contact/controllers/IndexController.php
@@ -3,10 +3,16 @@ class Contact_IndexController extends Zend_Controller_Action
 {
 	public function indexAction()
 	{
+        // Solução Provisória com Include
+		require_once APPLICATION_PATH . '/modules/contact/forms/Contact.php';
+		
 		$frmContact = new Contact_Form_Contact();
+		if ($this->_request->isPost() && $frmContact->isValid($_POST)) {
+			
+		}
 		$frmContact->setAction('/contact');
 		$frmContact->setMethod('post');
-		$this->view->form = $frmContact;
+		$this->view->Form = $frmContact;
 	}
 }
 ?>
