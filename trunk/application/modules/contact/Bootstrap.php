af38c8527d06e0f4051e4fe9ebf33b8f717cfb6a
renatomefidf <renatomefidf@ddd36b68-90d1-4cec-e3d0-770ee2dfee97>
2010-06-29 18:56:55 +0000

    Formulário de contato funcionando(ainda com include)

--- trunk/application/modules/contact/Bootstrap.php
+++ trunk/application/modules/contact/Bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 class Contact_Bootstrap extends Zend_Application_Module_Bootstrap
-{
+{   
 }
 ?>
--- trunk/application/modules/contact/controllers/IndexController.php
+++ trunk/application/modules/contact/controllers/IndexController.php
@@ -8,6 +8,30 @@ class Contact_IndexController extends Zend_Controller_Action
 		
 		$frmContact = new Contact_Form_Contact();
 		if ($this->_request->isPost() && $frmContact->isValid($_POST)) {
+			$sender = $frmContact->getValue('name');
+			$email = $frmContact->getValue('email');
+			$subject = $frmContact->getValue('subject');
+			$message = $frmContact->getValue('message');
+			
+			$htmlMessage = $this->view->partial(
+			     'templates/default.phtml',
+			     $frmContact->getValues()
+			);
+			
+			$mail = new Zend_Mail();
+			$mail->setSubject($subject);
+			$mail->setFrom($email,$sender);
+			$mail->addTo('contato@renatomefi.com.br','WebMaster');
+			$mail->setBodyHtml($htmlMessage);
+			$mail->setBodyText($message);
+			
+			$result = $mail->send();
+			$this->view->messageProcessed = true;
+			if ($result) {
+				$this->view->sendError = false;
+			} else {
+				$this->view->sendError = true;
+			}
 			
 		}
 		$frmContact->setAction('/contact');
