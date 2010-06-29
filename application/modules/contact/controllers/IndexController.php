a8330873d1cddb139e250819f73cd83d48724f2a
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2010-06-29 20:13:34 +0000

    contato 100% ok, no include

--- application/modules/contact/controllers/IndexController.php
+++ application/modules/contact/controllers/IndexController.php
@@ -21,9 +21,7 @@ class Contact_IndexController extends Zend_Controller_Action
 
 	public function indexAction()
 	{
-        // Solução Provisória com Include
-		require_once APPLICATION_PATH . '/modules/contact/forms/Contact.php';
-		
+
 		$frmContact = new Contact_Form_Contact();
 		if ($this->_request->isPost() && $frmContact->isValid($_POST)) {
 			$sender = $frmContact->getValue('name');
