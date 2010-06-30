1eccaf45b3ccce3c518536b2f1ba17dd37f27d10
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2010-06-30 13:17:08 +0000

    cache working

--- library/CMS/Application/Resource/Cache.php
+++ library/CMS/Application/Resource/Cache.php
@@ -1,17 +1,17 @@
 <?php
 class CMS_Application_Resource_Cache extends Zend_Application_Resource_ResourceAbstract
 {
-	public function init ()
-	{
-		$options = $this->getOptions();
-		$cache = Zend_Cache::factory(
-		      $options['frontEnd'],
-		      $options['backEnd'],
-		      $options['frontEndOptions'],
-		      $options['backEndOptions']
-		);
-		Zend_Registry::set('cache', $cache);
-		return $cache;
-	}
+    public function init ()
+    {
+        $options = $this->getOptions();
+        // Get a Zend_Cache_Core object
+        $cache = Zend_Cache::factory(
+            $options['frontEnd'], 
+            $options['backEnd'], 
+            $options['frontEndOptions'], 
+            $options['backEndOptions']);
+        Zend_Registry::set('cache', $cache);
+        return $cache;
+    }
 }
 ?>
