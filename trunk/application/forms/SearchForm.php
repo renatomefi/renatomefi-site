316a03165ee88a1ac2c6d2e4b52d8e49a7bf17b4
renatomefidf <renatomefidf@ddd36b68-90d1-4cec-e3d0-770ee2dfee97>
2010-07-02 01:08:02 +0000

    changing search position

--- trunk/application/forms/SearchForm.php
+++ trunk/application/forms/SearchForm.php
@@ -6,13 +6,26 @@ class Form_SearchForm extends Zend_Form
 	   $query = $this->createElement('text','query');
 	   $query->setRequired(true);
 	   $query->setAttrib('size',20);
+	   $query->addDecorator(array('div_d' => 'HtmlTag'),array('tag' => 'div'));
 	   $this->addElement($query);
 	   
 	   $submit = $this->createElement('submit','search');
 	   $submit->setLabel('Procurar');
-	   $submit->setDecorators(array('ViewHelper'));
+	   //$submit->setDecorators(array('ViewHelper'));
+	   $submit->setDecorators(array(
+		    'ViewHelper',
+		    'Description',
+		    array('HtmlTag', array('tag' => null)),
+		    array('Label', array('tag' => null)),
+		));
+		$this->addElement($submit);
+		$this->setDecorators(array(
+		    'FormElements',
+		    array('HtmlTag', array('tag' => 'div')),
+		    'Form',
+		));
 	   
-	   $this->addElement($submit);
+	   
 	}
 }
 
