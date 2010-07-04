637ab8dfd0215c6862dd0129a73a6622b78631e9
renatomefidf <renatomefidf@ddd36b68-90d1-4cec-e3d0-770ee2dfee97>
2010-07-04 22:54:09 +0000

    slider changes, appear only if is needed, better look

--- trunk/public_html/util/js/documentready.js
+++ trunk/public_html/util/js/documentready.js
@@ -16,4 +16,29 @@ $(document).ready(function() {
 		$.cookie('loginMenuState', 'close', { path: '/' });
 	}
 	
+	infoHeight = $("#info").height();
+
+	if (infoHeight > 780) {
+		
+		$(':range').attr('max',infoHeight);
+		
+	    var scroll = $("#info");
+	    
+	    $(":range").rangeinput({
+	        onSlide: function(ev, step)  {
+	            scroll.css({top: -step});
+	        },
+	        progress: true,
+	        value: 100,
+	        vertical: true,
+	        change: function(e, i) {
+	            scroll.animate({top: -i}, "fast");
+	        },
+	        speed: 0
+	    });
+	    $('.handle').css('top', '477px');
+	    $('.progress').css('height', '7px');
+	} else {
+		$(":range").css('display','none');
+	}
 });
