edbd103601779f2371c0d3356c3b0ea4f9875c43
renatomefidf <renatomefidf@ddd36b68-90d1-4cec-e3d0-770ee2dfee97>
2010-07-07 23:58:04 +0000

    lista controller and model complete.

--- trunk/application/modules/maladireta/controllers/ListaController.php
+++ trunk/application/modules/maladireta/controllers/ListaController.php
@@ -1,26 +1,66 @@
 <?php
-class Maladireta_ListaController extends Zend_Controller_Action
-{
-	public function indexAction()
+class Maladireta_ListaController extends Zend_Controller_Action {
+	public function indexAction() 
 	{
-		
+	   $mdlLista = new Maladireta_Model_Lista();
+	   $listas = $mdlLista->getListas();
+	   
+	   $this->view->listas = $listas;
 	}
 	
-	public function createAction()
+	public function createAction() 
 	{
-	   $frmLista = new Maladireta_Form_Lista();
-       
-	   $this->view->form = $frmLista;
+		$frmLista = new Maladireta_Form_Lista ();
+		$mdlLista = new Maladireta_Model_Lista ();
+		$auth = Zend_Auth::getInstance();
+		if ($this->_request->isPost ()) {
+			if ($frmLista->isValid ( $_POST )) {
+                $result = $mdlLista->createLista(
+                    $frmLista->getValue('nome'),
+                    $auth->getIdentity()->username,
+                    $frmLista->getValue('info')
+                );
+                if ($result) {
+                	$this->_redirect('/maladireta/lista');
+                }
+			}
+		}
+		
+		$this->view->form = $frmLista;
 	}
 	
-	public function editAction()
+	public function editAction() 
 	{
-		
+	    $frmLista = new Maladireta_Form_Lista ();
+        $mdlLista = new Maladireta_Model_Lista ();
+        $id = $this->_request->getParam('id');
+        $lista = $mdlLista->find($id)->current();
+        
+        if ($this->_request->isPost ()) {
+            if ($frmLista->isValid ( $_POST )) {
+                $result = $mdlLista->updateLista(
+                    $frmLista->getValue('id'),
+                    $frmLista->getValue('nome'),
+                    $frmLista->getValue('info')
+                );
+                if ($result) {
+                    $this->_redirect('/maladireta/lista');
+                }
+            }
+        }
+        $frmLista->populate($lista->toArray());
+        $this->view->form = $frmLista;
 	}
 	
-	public function deleteAction()
+	public function deleteAction() 
 	{
-		
+	   $id = $this->_request->getParam('id');
+	   $mdlLista = new Maladireta_Model_Lista();
+	   $result = $mdlLista->deleteLista($id);
+
+	   if ($result) {
+	   	   $this->_redirect('/maladireta/lista');
+	   }
 	}
 }
 ?>
