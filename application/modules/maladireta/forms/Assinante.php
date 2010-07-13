7e6099dc3bfdabc70634ef3506d653c421216c3d
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2010-07-13 01:18:34 +0000

    assinante mvc complete.

--- application/modules/maladireta/forms/Assinante.php
+++ application/modules/maladireta/forms/Assinante.php
@@ -9,11 +9,23 @@ class Maladireta_Form_Assinante extends Zend_Form
 		$lista_id = $this->createElement('hidden','lista_id');
         $this->addElement($lista_id);
         
+        $nome = $this->createElement('text','first_name');
+        $nome->setLabel('Nome: ');
+        $nome->setRequired(false);
+        $nome->setAttrib('size',40);
+        $this->addElement($nome);
+        
+        $snome = $this->createElement('text','last_name');
+        $snome->setLabel('Sobrenome: ');
+        $snome->setRequired(false);
+        $snome->setAttrib('size',40);
+        $this->addElement($snome);
+        
 		$email = $this->createElement('text','email');
 		$email->setLabel('E-mail: ');
 		$email->setRequired(true);
-		$email->addValidator('EmailAdress');
-        $email->addErrorMessage('E-mail inválido!');
+		//$email->addValidator('EmailAdress');
+        //$email->addErrorMessage('E-mail inválido!');
         $this->addElement($email);
         
         $info = $this->createElement('textarea','info');
